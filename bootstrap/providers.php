f7285bad54cc90aa1208a454183c111109ffa367
Itsmeeedanhil <djampatin@gmail.com>
2026-08-30 19:50:56 +0800

    Register ViewServiceProvider in bootstrap/providers.php to ensure view factory is bound

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
+    Illuminate\Events\EventServiceProvider::class,
+    Illuminate\Cookie\CookieServiceProvider::class,
+    Illuminate\Session\SessionServiceProvider::class,
+    Illuminate\Database\DatabaseServiceProvider::class,
+    Illuminate\Auth\AuthServiceProvider::class,
+    Illuminate\Filesystem\FilesystemServiceProvider::class,
+    Illuminate\Validation\ValidationServiceProvider::class,
 ];
